ace98692e1b2ee9cd5f945140be018b385d51c7d
Arda Akinci <aakinci@andrew.cmu.edu>
2024-11-20 13:16:02 -0500

    Basic Register Page Is Now In Tact

--- lumira/forms.py
+++ lumira/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Availability
+from .models import Availability, CustomUser
+from django.contrib.auth.forms import UserCreationForm
 
 class AvailabilityForm(forms.ModelForm):
     class Meta:
@@ -11,3 +12,14 @@ class AvailabilityForm(forms.ModelForm):
             'end_time': forms.TimeInput(attrs={'type': 'time'}),
         }
 
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'first_name', 'last_name', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if CustomUser.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already in use.")
+        return email
